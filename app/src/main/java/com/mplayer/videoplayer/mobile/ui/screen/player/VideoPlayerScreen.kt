7be5800d15d21d36e94da4aa05d48a4d65861ba2
Rahul Prasad <rahulprasad5943@gmail.com>
2026-05-22 20:11:33 +0530

    fix the mobile time show in hours

--- app/src/main/java/com/mplayer/videoplayer/mobile/ui/screen/player/VideoPlayerScreen.kt
+++ app/src/main/java/com/mplayer/videoplayer/mobile/ui/screen/player/VideoPlayerScreen.kt
@@ -1702,10 +1702,16 @@ enum class GestureType { BRIGHTNESS, VOLUME }
 enum class DragDirection { HORIZONTAL, VERTICAL }
 
 fun formatTime(ms: Long): String {
-    val totalSeconds = ms / 1000
-    val minutes = totalSeconds / 60
+    val safeMs = ms.coerceAtLeast(0)
+    val totalSeconds = safeMs / 1000
+    val hours = totalSeconds / 3600
+    val minutes = (totalSeconds % 3600) / 60
     val seconds = totalSeconds % 60
-    return "%02d:%02d".format(minutes, seconds)
+    return if (hours > 0) {
+        "%d:%02d:%02d".format(hours, minutes, seconds)
+    } else {
+        "%02d:%02d".format(minutes, seconds)
+    }
 }
 
 fun enterPictureInPicture(context: android.content.Context) {
